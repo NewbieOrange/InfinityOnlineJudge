475eec15125e85b0d9f31673dc7134461b531e73
NewbieOrange <yutiancheng1999@gmail.com>
2019-11-19 20:49:12 +0800

    Add new controllers to restful service

--- src/main/kotlin/xyz/chengzi/ooad/service/RestService.kt
+++ src/main/kotlin/xyz/chengzi/ooad/service/RestService.kt
@@ -2,17 +2,32 @@ package xyz.chengzi.ooad.service
 
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
-import xyz.chengzi.ooad.controller.SessionController
-import xyz.chengzi.ooad.controller.UserController
+import xyz.chengzi.ooad.controller.*
 import xyz.chengzi.ooad.server.ApplicationServer
 
 class RestService(server: ApplicationServer, private val port: Int) {
+    private val contestController = ContestController(server)
+    private val problemController = ProblemController(server)
+    private val submissionController = SubmissionController(server)
     private val userController = UserController(server)
     private val sessionController = SessionController(server)
+
     private val app = Javalin.create().routes {
         path("api") {
+            path("contests") {
+                get(contestController::listAll)
+                post(contestController::create)
+            }
+            path("problems") {
+                get(problemController::listAll)
+                post(problemController::create)
+            }
+            path("submissions") {
+                get(submissionController::listAll)
+                post(submissionController::create)
+            }
             path("users") {
-                post(userController::createUser)
+                post(userController::create)
             }
             path("user") {
                 get(userController::getCurrentUser)
